05ad681341dd28903009b0350a5d23d80731ebb9
Teimur Gasanov <teymurgg321@gmail.com>
2019-02-04 10:18:37 +0600

    #1 Update protocol

--- protocol/protocol.go
+++ protocol/protocol.go
@@ -1,21 +1,51 @@
 package protocol
 
-// NewPeerCode is a code for notification about a new client connected
-const NewPeerCode = 1
+const (
+	// NewPeerCode is a code for notification about a new client connected
+	NewPeerCode = 1
+	// ExitPeerCode is a code for notification about a client exited
+	ExitPeerCode = 2
+	// CheckFileCode is a code for checking file availability
+	CheckFileCode = 3
+	// GetChunkCode is a code for getting a chunk
+	GetChunkCode = 4
+)
 
-// ExitPeerCode is a code for notification about a client exited
-const ExitPeerCode = 2
+const (
+	// FileNotExistStatus is a status when requested file does not exist
+	FileNotExistStatus = 0
+	// FileExistStatus is a status when requested file exists
+	FileExistStatus = 1
+	// ChunkNotSentStatus is a status when requested chunk could not be sent
+	ChunkNotSentStatus = 2
+	// ChunkSentStatus is a status when requested chunk was sent
+	ChunkSentStatus = 3
+)
+
+// RequestInfo is meta data of a request
+type RequestInfo struct {
+	FileName   string
+	ChunkIndex int64
+}
 
 // Request is a protocol request model
 type Request struct {
-	Code     int64
-	Data     string
-	MetaData string
+	Code int64
+	Data string
+	Info RequestInfo
+}
+
+// ResponseInfo is meta data of a response
+type ResponseInfo struct {
+	FileName                string
+	FileHash                string
+	ChunkSize               int64
+	ChunkBytesStartPosition int64
 }
 
 // Response is a protocol response model
 type Response struct {
-	Code     int64
-	Data     string
-	MetaData string
+	Status int64
+	Data   string
+	Info   ResponseInfo
 }
